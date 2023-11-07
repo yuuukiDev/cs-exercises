5f95a5ebbc366822862ef780679af3889382695f
yuuukiDev <mohamedemadeldin34@gmail.com>
2023-11-07 23:00:36 +0200

    First .NET App

---
+++ FirstMVCApp/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
